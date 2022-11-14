154d1ee460bd1ef50e1e5502cf2819e8eeaaf9d1
murilo-alvesmelo <murilo.melo72@gmail.com>
2022-11-14 10:22:16 -0300

    Add url api diel-backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'https://diel-backend.herokuapp.com/',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 });
